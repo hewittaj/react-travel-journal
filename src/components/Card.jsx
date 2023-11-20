b1f0bdefeea0f92d7be8c4ce00639d49441af389
Alex Hewitt <alexhewitt.j@gmail.com>
2023-11-20 13:20:14 -0600

    add card details and update style settings

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import mapPin from '../../public/assets/map-pin.svg'
 
 function Card(props) {
     console.log(props.item.img)
     return (
         <div className="card">
             <img src={`/public/assets/${props.item.img}`} className='card-photo'/>
-            
+            <div className='card-details'>
+                <div className="card-location-info">
+                    <img src={mapPin}/>
+                    <p>{props.item.country}</p>
+                    <a href={props.item.googleMapsLink}>View on Google Maps</a>
+                </div>
+                <div className='card-location-details'>
+
+                </div>
+            </div>
         </div>
     )
 }
