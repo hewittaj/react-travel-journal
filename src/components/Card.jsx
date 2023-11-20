b6f3c2aa96a0d67778776e453742ae5e79323cae
Alex Hewitt <alexhewitt.j@gmail.com>
2023-11-20 13:50:40 -0600

    adjust image locations for netlify

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import mapPin from '../../public/assets/map-pin.svg'
+import mapPin from '../../public/assets/img/map-pin.svg'
 
 function Card(props) {
     console.log(props.item.img)
     return (
         <div className="card">
-            <img src={`/public/assets/${props.item.img}`} className='card-photo'/>
+            <img src={`/public/assets/img/${props.item.img}`} className='card-photo'/>
             <div className='card-details'>
                 <div className="card-location-info">
                     <img src={mapPin}/>
